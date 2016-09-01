2e49bf7fc5f0df6b0995cc8f9e0b560acd0858a7
Victor Moreno <thecodingteacher@gmail.com>
2016-09-01 16:38:43 -0400

    docs: added translations to joe's questions

--- web1010-test/test02.js
+++ web1010-test/test02.js
@@ -1,3 +1,18 @@
+//Question 1.1
+//Find the error in the following for loop:
+
+for (var i = 0; i < '5'; i++;) {
+ console.log('i')
+}
+
+//Question 1.2
+//Find the error in the following conditional:
+ if false{
+   consola.log("hello")
+ }else{
+   console.log("hasta luego")
+ }
+
 //Question 1.3
 //Find the error in the following code:
 var x = 15;
@@ -10,6 +25,21 @@ if (x > 10){
 var y = 27;
 if y === 27
   console.log(27);
+
+//Question 2.1
+//What does will the console output when the following is entered.
+ '1'+1
+
+a. 2
+b. "2"
+c. "11"
+d. 11
+
+//Question 2.2
+//Find the errors in the following
+var name = "Roberto"
+var time =3
+name"+ said "He's comming late.", the meetin is still at " + time +
 
 //Question 2.3
 //What is the value of x?
@@ -26,6 +56,19 @@ var y = 6;
 x = x*y;
 var z = 4;
 x = x*z;
+
+
+//Question 3.1
+//Write a sentence that uses the following variables
+var firstName = "John"
+var lastName = "Martinez"
+var city = "Miami"
+
+
+//Question 3.2
+//Write a sentence that uses the following variables
+totalPower = 32
+generator = 1
 
 
 //Question 3.3
@@ -64,7 +107,11 @@ var interest = 0.05;
 var years = 30;
 
 //Extra credit
-//given three variables, create a program that outputs (to the console) "You should buy a new car" only if the current car age is greater than 10, the mileage is greater than 150,000 and the brand is Kia. Otherwise, your program should output "Hold off a bit, be frugal."
+/*
+Dadas tres variables, escriba un programa que muestre (en la consola) "Usted debe comprar un carro nuevo" solamente si la eddad del carro que tiene (carAge) es mayor que 10, el millaje (mileage) es mayor que 150,000, y si la marca es Kia. De otra manera, su programa debe mostrar "Espere un poco, sea frugal."
+
+Given three variables, create a program that outputs (to the console) "You should buy a new car" only if the current car age is greater than 10, the mileage is greater than 150,000 and the brand is Kia. Otherwise, your program should output "Hold off a bit, be frugal."
+*/
 var carAge = 10;
 var mileage = 150000;
 var brand = "Kia";
@@ -77,113 +124,53 @@ else{
 }
 
 
-//Question 1.1
-//Find the error in the following for loop:
-
-// for (var i = 0; i < '5'; i++;) {
-//   console.log('i')
-// }
-//
-
-//Question 1.2
-//Find the error in the following conditional:
-// if false{
-//   consola.log("hello")
-// }else{
-//   console.log("hasta luego")
-// }
-
-//Question 2.1
-//What does will the console output when the following is entered.
-// '1'+1
-//a. 2
-//b. "2"
-//c. "11"
-//d. 11
-
-//Question 2.2
-//Find the errors in the following
-var name = "Roberto"
-var time =3
-// name"+ said "He's comming late.", the meetin is still at " + time +
-
-
-//Question 3.1
-//Write a sentence that uses the following variables
-var firstName = "John"
-var lastName = "Martinez"
-var city = "Miami"
-
-
-//Question 3.2
-//Write a sentence that uses the following variables
-totalPower = 32
-generator = 1
-
-
 //Question 5.2
-//Usted trbaja para un parque de agua. En esta parque hay 7 piscinas pequenas y 5 grandes.
-//El ingeniero le reporta todo los dias de cuales piscinas estan trabajando.
-//La contadora le preguntu que le haga un report de cuanto galones gasta por dia,
-//que depende en las piscinas operacionales.  Hoy toda las piscinas estan trbajando.
+/*
+You work for a water park. This park has 7 small pools and 5 large ones.
+The engineer tells you every day which pool is working.
+The accountant asked you to build a report which shows how many gallons are used per day in filling the pools, which depends on the pools that are operational. Today, every pool is operational.
+
+Usted trabaja para un parque de agua. En este parque hay 7 piscinas pequenas y 5 grandes.  
+El ingeniero le reporta todo los dias de cuales piscinas estan trabajando.
+La contadora le pidio que haga un reporte de cuanto galones gasta por dia, que depende en las piscinas operacionales.  Hoy toda las piscinas estan trabajando.
+*/
 
 var totalPools = 12
 var currentPool = 1
 var smallVolume = 1000
 var largeVolume = 3000
 var totalVolume = 0
-for (currentPool; currentPool< totalPools; currentPool++) {
-  if(currentPool <= 7){
-    totalVolume += smallVolume
-    console.log("Pool #" + currentPool +  " is on and has been filled with " +totalVolume +" gallons." )
-  } else{
-    totalVolume += largeVolume
-    console.log("Pool #" + currentPool +  " is on and has been filled with " +totalVolume +" gallons." )
-  }
+for (; ; ) {
+  
+  
 }
+
 
 // Extra Credit
-// Este dia la picina numero 2 10 no trabajan,
-
-for (currentPool; currentPool<= totalPools; currentPool++) {
-  if(currentPool <= 7){
-    if(currentPool == 2){
-      console.log("Pool #" + currentPool +  "is not open today"  )
-    } else{
-      totalVolume += smallVolume
-      console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
-    }
-
-  } else{
-    if(currentPool == 10){
-      console.log("Pool #" + currentPool +  " is not open today" )
-    }else{
-      totalVolume += largeVolume
-      console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
-    }
-
-  }
-}
-
-for (currentPool; currentPool<= totalPools; currentPool++){
-  if(currentPool <= 7 && currentPool==2){
-    console.log("Pool #" + currentPool +  " is not open today"  )
-  }
-  else if(currentPool <= 7){
-    totalVolume += smallVolume
-    console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
-  } else if(currentPool == 10){
-    console.log("Pool #" + currentPool +  " is not open today" )
-  } else {
-      totalVolume += largeVolume
-      console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
-  }
-
+// Este dia las picsinas numero 2 y 10 no trabajan,
+// Repeat the same problem but assume pools 2 and 10 aren't operational.
+var totalPools = 12
+var currentPool = 1
+var smallVolume = 1000
+var largeVolume = 3000
+var totalVolume = 0
+for ( ;   ;  ) {
+  
+  
+  
+  
+  
 }
 
 
 
 //Extra Credit
-//El ingeniero sabe que manana la 2, 6, y 10 no van a trbajar.
-//Ademas, la picina numero 12 la van a llenar con la mitad del volumen, para ninos pequenos.
-//Adelantese y haga el reporta hoy, para que manana descanse
+/*
+The engineer knows that pools 2, 6 and 10 won't work tomorrow.
+Besides, pool number 12 will be filled with half the volume in order to accommodate children.
+Go ahead and build tomorrow's report today, so you can rest easy.
+
+El ingeniero sabe que manana las picsinas 2, 6, y 10 no van a trabajar.
+Ademas, la picina numero 12 la van a llenar con la mitad del volumen, para ninos pequenos.
+Adelantese y haga el reporta hoy, para que manana descanse
+*/
