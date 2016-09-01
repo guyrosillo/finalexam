bc67fccb86aa911828fc81b36910eb0d5e3c6247
wordyallen <joe.narvaez@gmil.com>
2016-09-01 15:49:58 -0400

    added more questions

--- web1010-test/test02.js
+++ web1010-test/test02.js
@@ -75,3 +75,115 @@ if (         ){
 else{
   console.log("Hold off a bit, be frugal.");
 }
+
+
+//Question 1.1
+//Find the error in the following for loop:
+
+// for (var i = 0; i < '5'; i++;) {
+//   console.log('i')
+// }
+//
+
+//Question 1.2
+//Find the error in the following conditional:
+// if false{
+//   consola.log("hello")
+// }else{
+//   console.log("hasta luego")
+// }
+
+//Question 2.1
+//What does will the console output when the following is entered.
+// '1'+1
+//a. 2
+//b. "2"
+//c. "11"
+//d. 11
+
+//Question 2.2
+//Find the errors in the following
+var name = "Roberto"
+var time =3
+// name"+ said "He's comming late.", the meetin is still at " + time +
+
+
+//Question 3.1
+//Write a sentence that uses the following variables
+var firstName = "John"
+var lastName = "Martinez"
+var city = "Miami"
+
+
+//Question 3.2
+//Write a sentence that uses the following variables
+totalPower = 32
+generator = 1
+
+
+//Question 5.2
+//Usted trbaja para un parque de agua. En esta parque hay 7 piscinas pequenas y 5 grandes.
+//El ingeniero le reporta todo los dias de cuales piscinas estan trabajando.
+//La contadora le preguntu que le haga un report de cuanto galones gasta por dia,
+//que depende en las piscinas operacionales.  Hoy toda las piscinas estan trbajando.
+
+var totalPools = 12
+var currentPool = 1
+var smallVolume = 1000
+var largeVolume = 3000
+var totalVolume = 0
+for (currentPool; currentPool< totalPools; currentPool++) {
+  if(currentPool <= 7){
+    totalVolume += smallVolume
+    console.log("Pool #" + currentPool +  " is on and has been filled with " +totalVolume +" gallons." )
+  } else{
+    totalVolume += largeVolume
+    console.log("Pool #" + currentPool +  " is on and has been filled with " +totalVolume +" gallons." )
+  }
+}
+
+// Extra Credit
+// Este dia la picina numero 2 10 no trabajan,
+
+for (currentPool; currentPool<= totalPools; currentPool++) {
+  if(currentPool <= 7){
+    if(currentPool == 2){
+      console.log("Pool #" + currentPool +  "is not open today"  )
+    } else{
+      totalVolume += smallVolume
+      console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
+    }
+
+  } else{
+    if(currentPool == 10){
+      console.log("Pool #" + currentPool +  " is not open today" )
+    }else{
+      totalVolume += largeVolume
+      console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
+    }
+
+  }
+}
+
+for (currentPool; currentPool<= totalPools; currentPool++){
+  if(currentPool <= 7 && currentPool==2){
+    console.log("Pool #" + currentPool +  " is not open today"  )
+  }
+  else if(currentPool <= 7){
+    totalVolume += smallVolume
+    console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
+  } else if(currentPool == 10){
+    console.log("Pool #" + currentPool +  " is not open today" )
+  } else {
+      totalVolume += largeVolume
+      console.log("Pool #" + currentPool +  " is on and has been filled with" +totalVolume  )
+  }
+
+}
+
+
+
+//Extra Credit
+//El ingeniero sabe que manana la 2, 6, y 10 no van a trbajar.
+//Ademas, la picina numero 12 la van a llenar con la mitad del volumen, para ninos pequenos.
+//Adelantese y haga el reporta hoy, para que manana descanse
