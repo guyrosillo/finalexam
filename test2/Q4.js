fc2cfa1ea94352a573a457b63ef6765dde9c4b2f
Brenton Cozby <brenton.cozby@gmail.com>
2016-12-16 12:21:40 -0500

    Create test 2

---
+++ test2/Q4.js
@@ -0,0 +1,2 @@
+// Log to the console any expression that uses the modulo operator % and results
+// in the number 1.
---
+++ test2/Q9.js
@@ -0,0 +1,7 @@
+// Create a countMessage variable and assign to it the string below, but
+// replace the 3 in the string below with the numPassengers variable using
+// concatenation with the + operator.
+//
+//    Attention: There are now 3 passengers on the train!
+//
+// Then log countMessage to the console.
