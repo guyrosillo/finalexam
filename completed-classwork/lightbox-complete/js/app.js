b086bf0d4a6d2f32f4365ac2d431c5f48f386ea6
Brenton Cozby <brenton.cozby@gmail.com>
2017-02-14 22:05:13 -0500

    replace jquery with javascript native

--- completed-classwork/lightbox-complete/js/app.js
+++ completed-classwork/lightbox-complete/js/app.js
@@ -1,33 +1,42 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
 
-var $overlay = $('<div id="overlay"></div>');
-var $image = $('<img>');
-var $caption = $('<p></p>');
-
-//2. Add overlay
-$('body').append($overlay);
-//2.1 An image to overlay
-$overlay.append($image);
-//2.2 A caption
-$overlay.append($caption);
+var overlay = document.querySelector('.overlay');
+var figure = document.querySelector('figure');
+var figureImg = document.querySelector('figure img');
+var figcaption = document.querySelector('figure figcaption');
 
 //1. Capture the click event on a link to an image
-$("#imageGallery a").click(function(event){
-  event.preventDefault();
-  var imageLocation = $(this).attr('href');
-  //1.2 Update overlay with the image linked in the link
-  $image.attr('src', imageLocation);
-  //1.3 Get child's alt attribute and set caption
-  var captionText = $(this).children('img').attr('title');
-  $caption.text(captionText);
+document.querySelectorAll("#imageGallery a").forEach(function(anchor) {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        //1.1 Show the overlay.
+        overlay.classList.toggle('show');
+        //1.2 Update overlay with the image linked in the link
+        //1.3 Get child's alt attribute and set caption
+        var alt = event.target.getAttribute('alt');
+        var src = event.target.getAttribute('src');
 
-  //1.1 Show the overlay.
-  $overlay.show();
+        figureImg.setAttribute('alt', alt);
+        figureImg.setAttribute('src', src);
+        figcaption.innerHTML = alt;
+
+        figure.classList.toggle('show');
+    });
 });
 
+overlay.addEventListener('click', function() {
+    overlay.classList.remove('show');
+    figure.classList.remove('show');
+});
+
+figure.addEventListener('click', function() {
+    overlay.classList.remove('show');
+    figure.classList.remove('show');
+});
+
+//2. Add overlay
+//2.1 An image
+//2.2 A caption
 //3. When overlay is clicked
-$overlay.click(function() {
-    //3.1 Hide the overlay
-    $overlay.hide();
-});
+//3.1 Hide the overlay
