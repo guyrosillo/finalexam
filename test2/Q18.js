ce36100875b104b90c3914c8efb4ca9a2d1f9cbd
swolebrain <thecodingteacher@gmail.com>
2017-07-10 15:37:30 -0400

    fix: merge conflict

--- test2/Q18.js
+++ test2/Q18.js
@@ -2,11 +2,7 @@
 // Give your sum function two parameters called a and b.
 // Inside the body of the sum function, return the sum of a and b.
 //
-<<<<<<< HEAD
-// Then make 3 console logs by calling your sum function inside of them.
-=======
 // Then make 3 console logs calling (running) your sum function inside of them.
->>>>>>> 56053ac5a3103c3e39ace25be4b59075dfb783d1
 // Then first console log should print the number 8.
 // Then first console log should print the number 13.
 // Then first console log should print the number -3.
