ff153ae91ce222ce6bacb7cc263f0f9dcc6e58c5
Brenton Cozby <brenton.cozby@gmail.com>
2017-09-05 19:13:46 -0400

    use familiar/basic code

--- final-alt-2/script.js
+++ final-alt-2/script.js
@@ -1,8 +1,11 @@
 // no API key required for this API
-const api_url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?'
+const API_URL = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?'
 
 function getQuote() {
-    $.getJSON(api_url)
+    $.ajax({
+        url: API_URL,
+        dataType: 'json'
+    })
     .done(function (response) {
         console.log(response);
 
